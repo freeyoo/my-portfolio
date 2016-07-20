c91a44668b527f9e0e8df22bc594bc8b902afe36
stealdworks <stephine.aldea@gmail.com>
2016-07-21 00:29:19 +0800

    update the whole index file

--- index.php
+++ index.php
@@ -27,64 +27,25 @@
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" class="index" data-offset="500">
 
-  
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-               <a class="navbar-brand page-scroll" href="#myPage"><img src="img/newlogo.png" class="logo"></a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="hidden">
-                        <a href="#page-top"></a>
-                    </li>
-                    <li>
-                        <a  href="#what-section">Home</a>
-                    </li>
-                    <li>
-                        <a  href="#why-section">Works</a>
-                    </li>
-                    <li>
-                        <a  href="#testimonial-section">Blog</a>
-                    <li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
-
-
 
 
 <header class="hd-banner">
   <div class="container align-center header-text">
-    <h1 class="ht-lg-1 ">-Hello!-</h1>
-    <hr class="style-2">
-    <h1 class="ht-lg-2 ">-Hello!-</h1>
+    <h1 class="ht-lg-1 ">Hello!</h1>
+    <h1 class="ht-lg-2 color-aqua">I'm Stephine</h1>
+    <img src="img/meinbanner.jpg" class="avatar-in-banner">
+    <p class="color-lightorange">Web Designer / Web Developer</p>
     
-  
+    
   </div>
 </header>
 
 <div id="why-section">
 <section class="why-section">
   <div class="container align-center"> <!-- container start -->
-    <h1 class="ht-lg-2 color-lightgrey">WHY WE ARE DOING THIS</h1>
+    <h1 class="ht-lg-2 color-lightgrey">WHAT I CAN DO?</h1>
     <hr class="style-2">
-    <p class="color-lightgrey">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit fringilla ex a faucibus. Integer mollis convallis arcu, vitae tincidunt est dapibus quis. Sed sit amet quam nulla. Suspendisse vel facilisis diam. Vivamus et neque dapibus, imperdiet mauris vel, rhoncus orci. Etiam a urna mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit fringilla ex a faucibus. Integer mollis convallis arcu, vitae tincidunt est dapibus quis. Sed sit amet quam nulla. Suspendisse vel facilisis diam. Vivamus et neque dapibus, imperdiet mauris vel, rhoncus orci. Etiam a urna miVivamus et neque dapibus, imperdiet mauris vel, rhoncus orci. Etiam a urna mi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque blandit fringilla ex a faucibus. Integer mollis convallis arcu, vitae tincidunt est dapibus quis. Sed sit amet quam nulla. Suspendisse vel facilisis diam. Vivamus et neque dapibus, imperdiet mauris vel, rhoncus orci. Etiam a urna m.</p>
-    <br/>
+   
     <div class="row slideanim">
       <div class="col-xs-offset-2 col-xs-8 col-sm-offset-0 col-sm-4 col-md-4  ">
         <img src="img/gear-icon.png" class="custom-img-responsive">
@@ -110,86 +71,12 @@
 
 </div>
 
+    
 
-<section id="location-section" class="google-map">
-<div class="container">
-   <div class="section-header">
-            <h1 class="ht-lg-2 color-grey">OUR LOCATION</h1>
-            <hr class="style-2">
-    </div>
-</div>
-   
-
-<div id="googleMap" style="height:600px;width:100%;"></div>
-  
-
-</section>
-
-<script src="http://maps.googleapis.com/maps/api/js"></script>
-<script>
-        var myCenter=new google.maps.LatLng(1.280846,103.820259);
-
-        function initialize()
-        {
-        var mapProp = {
-          center:myCenter,
-          zoom:17,
-          scrollwheel:false,
-          draggable:true,
-          mapTypeId:google.maps.MapTypeId.ROADMAP
-          };
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-        var marker=new google.maps.Marker({
-          position:myCenter,
-          });
-
-        marker.setMap(map);
-
-        var infowindow = new google.maps.InfoWindow({
-          content:"<h1 class='ht-md-1 align-center color-orange'>HEADQUARTERS</h1> <p>#04-02, 205 Henderson Road <br/> Henderson Industrial Park 159549. <p/> "
-          });
-        infowindow.open(map,marker);
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-          });
-        }
-        google.maps.event.addDomListener(window, 'load', initialize);
-</script>     
-
-<footer>
-  <section id="contacts-section"class="contact-section">
-  <div class="container header-text window-content-2 ">
-  <div class="section-header">
-            <h1 class="ht-lg-2 color-orange">CONTACT US</h1>
-            <hr class="style-1">
-        </div>
-      <div class="row">
-        <div class="col-md-offset-1 col-md-10">
-            <div class="row">
-                <div class=" col-md-6">
-                    <h1 class="ht-md-1 color-orange">SOCIAL MEDIA</h1>
-                </div>
-              <div class=" col-md-6">
-                <h1 class="ht-md-1 color-orange">PHONE</h1>
-                    <h2>Tel: +65 6334 4022</h2>
-                <h1 class="ht-md-1 color-orange">EMAIL</h1>
-                    <h2>Contactus@tabjoy.org</h2>
-                <h1 class="ht-md-1 color-orange">ADDRESS</h1>
-                    <h2>205 Henderson Rd.,<br>
-                    #07-02 Henderson Ind. Pk.
-                    </h2>
-              </div>
-            </div>
-        </div>
-      </div>
-  </div>
-  </section>
   <section class="footer-in-wrapper">
-      <p>Alrights Reserved 2016 | TABERNACLE OF JOY - FILIPINO JOY FELLOWSHIP | Designed by STEALDWORKS</p>
+      <p>Alrights Reserved 2016 / Stephine Miniz Aldea / stealdworks</p>
   </section>
-</footer>
+
 
 <!--LOAD SCRIPTS -->
 
@@ -198,40 +85,7 @@
 <!--<script src="js/jquery.easing.min.js"></script> -->
 <script src="js/scrolling-nav.js"></script> 
 
-<script>
-$(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-   
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-  });
-  
-  $(window).scroll(function() {
-    $(".slideanim").each(function(){
-      var pos = $(this).offset().top;
-
-      var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-    });
-  });
-})
-</script>
 
 </body>
 </html>
